9168c858f5acbb4e62006c18e50043276e8ea5ab
ajayrahul <ajayrahulr.0@gmail.com>
2025-11-09 17:44:02 +0530

    Modified commit

--- GitLearn1/src/GITUSERMAIN.java
+++ GitLearn1/src/GITUSERMAIN.java
@@ -3,5 +3,8 @@ public class GITUSERMAIN {
 
 	public static void main (String args[]) {
 		System.out.print("User1");
+		System.out.print("Modifying");
+		System.out.print("GitDemo");
+		System.out.print("not adding");
 	}
 }
